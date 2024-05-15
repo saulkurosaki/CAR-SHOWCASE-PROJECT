85211433e7588c6d212dceef071b80a7defa9223
saulkurosaki <saulkurosaki13@gmail.com>
2024-05-15 17:31:00 -0600

    feat(CustomButton): implement the CustomButtonProps interface within the component

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -1,15 +1,22 @@
 "use client";
 
+import { CustomButtonProps } from "@/types";
 import Image from "next/image";
 
-const CustomButton = () => {
+const CustomButton = ({
+  title,
+  containerStyles,
+  handleClick,
+}: CustomButtonProps) => {
   return (
     <button
       disabled={false}
       type={"button"}
       className={"custom-btn"}
       onClick={() => {}}
-    ></button>
+    >
+      <span className={"flex-1"}>Title</span>
+    </button>
   );
 };
 
