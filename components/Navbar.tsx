d60568239bd9c839fd81f94ce76abe2363d6e310
saulkurosaki <saulkurosaki13@gmail.com>
2024-05-15 17:52:35 -0600

    feat(Navbar): implement the navbar Link component props

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,15 +7,15 @@ const Navbar = () => {
   return (
     <header className="w-full absolute z-10">
       <nav className="max-w-[1440px] mx-auto flex justify-between items-center sm:p-16 p-6 py-4">
-        {/* <Link href="/" className="flex justify-center items-center">
-          <Image
+        <Link href="/" className="flex justify-center items-center">
+          {/* <Image
             src="/logo.svg"
             alt="Car Hub logo"
             width={118}
             height={18}
             className="object-contain"
-          />
-        </Link> */}
+          /> */}
+        </Link>
       </nav>
     </header>
   );
