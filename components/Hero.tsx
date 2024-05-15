4fb703d881cab79f8917f7a665c5e464e0e06025
saulkurosaki <saulkurosaki13@gmail.com>
2024-05-15 17:16:41 -0600

    feat(Hero): implement the component subtitle

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -7,6 +7,11 @@ const Hero = () => {
         <h1 className="hero__title">
           Find, book, or rent a car — quickly and easily!
         </h1>
+
+        <p className="hero__subtitle">
+          Streamline your car rental experience with our effortless booking
+          process.
+        </p>
       </div>
     </div>
   );
