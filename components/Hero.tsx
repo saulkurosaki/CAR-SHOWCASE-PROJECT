94a850e71b0ca9277bd04d790d2a1247f894219e
saulkurosaki <saulkurosaki13@gmail.com>
2024-05-15 17:39:28 -0600

    feat(Hero): implement the hero section image container layout

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import React from "react";
+import Image from "next/image";
+
 import CustomButton from "./CustomButton";
 
 const Hero = () => {
@@ -23,6 +25,12 @@ const Hero = () => {
           containerStyles="bg-primary-blue text-white rounded-full mt-10"
           handleClick={handleScroll}
         />
+      </div>
+
+      <div className="hero__image-container">
+        <div className="hero__image">
+          {/* <Image src="/hero.png" alt="hero" fill className="object-contain" /> */}
+        </div>
       </div>
     </div>
   );
