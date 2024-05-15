43a0201cac32317d7cdee38e052016dc159dcce6
saulkurosaki <saulkurosaki13@gmail.com>
2024-05-15 17:53:01 -0600

    feat(Navbar): implement the navbar project logo

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -8,13 +8,13 @@ const Navbar = () => {
     <header className="w-full absolute z-10">
       <nav className="max-w-[1440px] mx-auto flex justify-between items-center sm:p-16 p-6 py-4">
         <Link href="/" className="flex justify-center items-center">
-          {/* <Image
+          <Image
             src="/logo.svg"
             alt="Car Hub logo"
             width={118}
             height={18}
             className="object-contain"
-          /> */}
+          />
         </Link>
       </nav>
     </header>
