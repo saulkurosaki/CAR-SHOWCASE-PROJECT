29350d537e475418d2587b3329867c4c7a092c7a
saulkurosaki <saulkurosaki13@gmail.com>
2024-05-16 14:29:31 -0600

    feat(SearchManufacturer): implement the headlessui ComboboxButton component as well as its image

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,8 +1,14 @@
 "use client";
 
-import { Combobox, Transition } from "@headlessui/react";
+import {
+  Combobox,
+  ComboboxButton,
+  ComboboxInput,
+  Transition,
+} from "@headlessui/react";
 
 import { SearchManufacturerProps } from "@/types";
+import Image from "next/image";
 
 const SearchManufacturer = ({
   manufacturer,
@@ -11,7 +17,17 @@ const SearchManufacturer = ({
   return (
     <div className="search-manufacturer">
       <Combobox>
-        <div className="relative w-full"></div>
+        <div className="relative w-full">
+          <ComboboxButton className="absolute top-[14px]">
+            <Image
+              src="/car-logo.svg"
+              alt="Car Logo"
+              width={20}
+              height={20}
+              className="ml-4"
+            />
+          </ComboboxButton>
+        </div>
       </Combobox>
     </div>
   );
