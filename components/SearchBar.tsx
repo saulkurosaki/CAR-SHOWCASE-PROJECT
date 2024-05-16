5b40ff931df858473bd1de9bbc264e64f1d9bc87
saulkurosaki <saulkurosaki13@gmail.com>
2024-05-16 13:29:57 -0600

    feat(SearchManufacturer): implement the SearchManufacturer component initial layout

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,11 +1,15 @@
 "use client";
 
+import SearchManufacturer from "./SearchManufacturer";
+
 const SearchBar = () => {
   const handleSubmit = () => {};
 
   return (
     <form className="searchbar" onSubmit={handleSubmit}>
-      <div className="searchbar__item"></div>
+      <div className="searchbar__item">
+        <SearchManufacturer />
+      </div>
     </form>
   );
 };
