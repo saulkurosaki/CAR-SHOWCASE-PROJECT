c0c9571e5e2a3ea7c504c6c61986c4ad5ded91f6
saulkurosaki <saulkurosaki13@gmail.com>
2024-05-16 14:24:54 -0600

    feat(SearchManufacturer): implement the headlessui Combobox component along as its layout

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,11 +1,20 @@
+"use client";
+
+import { Combobox, Transition } from "@headlessui/react";
+
 import { SearchManufacturerProps } from "@/types";
-import React from "react";
 
 const SearchManufacturer = ({
   manufacturer,
   setManufacturer,
 }: SearchManufacturerProps) => {
-  return <div>SearchManufacturer</div>;
+  return (
+    <div className="search-manufacturer">
+      <Combobox>
+        <div className="relative w-full"></div>
+      </Combobox>
+    </div>
+  );
 };
 
 export default SearchManufacturer;
