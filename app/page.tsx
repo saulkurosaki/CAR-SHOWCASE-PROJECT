6ffe73f9ead1fe97cea325b3e70242aa50ca5b1a
saulkurosaki <saulkurosaki13@gmail.com>
2024-05-15 20:18:22 -0600

    feat(page.tsx): implement the discover section upper division container layout

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,10 @@ export default function Home() {
   return (
     <main className="overflow-hidden">
       <Hero />
+
+      <div className="mt-12 padding-x padding-y max-width" id="discover">
+        <div className="home__text-container"></div>
+      </div>
     </main>
   );
 }
