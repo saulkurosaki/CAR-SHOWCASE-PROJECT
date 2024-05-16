84961b18bd86057605c24448f33d9e60102f778a
saulkurosaki <saulkurosaki13@gmail.com>
2024-05-15 20:22:11 -0600

    feat(page.tsx): implement the discover section upper division custom filters container layout

--- app/page.tsx
+++ app/page.tsx
@@ -11,6 +11,15 @@ export default function Home() {
           <h1 className="text-4xl font-extrabold">Car Catalogue</h1>
           <p>Explore the cars you might like</p>
         </div>
+
+        <div className="home__filters">
+          <SearchBar />
+
+          <div className="home__filter-container">
+            <CustomFilter title="fuel" />
+            <CustomFilter title="year" />
+          </div>
+        </div>
       </div>
     </main>
   );
