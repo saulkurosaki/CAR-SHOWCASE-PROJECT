81f95e2f7976e45812b96a253caf573a2f80a44d
saulkurosaki <saulkurosaki13@gmail.com>
2024-05-17 15:52:10 -0600

    feat(utils/index.ts): implement the complete car api fetch function

--- utils/index.ts
+++ utils/index.ts
@@ -1,16 +1,17 @@
-const url = "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla";
-const options = {
-  method: "GET",
-  headers: {
+export async function fetchCars() {
+  const headers = {
     "X-RapidAPI-Key": "f2475a1f79mshc0533298d992273p1b4b04jsn02fa86c6678a",
     "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
-  },
-};
+  };
 
-try {
-  const response = await fetch(url, options);
-  const result = await response.text();
-  console.log(result);
-} catch (error) {
-  console.error(error);
+  const response = await fetch(
+    "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars",
+    {
+      headers: headers,
+    }
+  );
+
+  const result = await response.json();
+
+  return result;
 }
