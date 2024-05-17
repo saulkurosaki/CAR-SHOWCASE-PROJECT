45705bf02ea46c6b512a73f2cf8d31924d7d8e27
saulkurosaki <saulkurosaki13@gmail.com>
2024-05-17 16:39:24 -0600

    feat(CarCard): implement the component contaier layout

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -4,13 +4,24 @@ import { useState } from "react";
 import Image from "next/image";
 
 import { CarProps } from "@/types";
+import CustomButton from "./CustomButton";
 
 interface CarCardProps {
   car: CarProps;
 }
 
 const CarCard = ({ car }: CarCardProps) => {
-  return <div>CarCard</div>;
+  const { city_mpg, year, make, model, transmission, drive } = car;
+
+  return (
+    <div className="car-card group">
+      <div className="car-card__content">
+        {/* <h2 className="car-card__content-title">
+          {make} {model}
+        </h2> */}
+      </div>
+    </div>
+  );
 };
 
 export default CarCard;
