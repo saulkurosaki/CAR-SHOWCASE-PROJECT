f6ea841804a755a9f6adba28a71a6a03fe41d58a
saulkurosaki <saulkurosaki13@gmail.com>
2024-05-17 16:18:00 -0600

    feat(Page.tsx): implement the car results error display

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 export default async function Home() {
   const allCars = await fetchCars();
 
-  console.log(allCars);
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
 
   return (
     <main className="overflow-hidden">
@@ -25,6 +25,15 @@ export default async function Home() {
             <CustomFilter title="year" />
           </div>
         </div>
+
+        {!isDataEmpty ? (
+          <section>WE HAVE CARS</section>
+        ) : (
+          <div className="home__error-container">
+            <h2 className="text-black text-xl font-bold">Oops, no results</h2>
+            <p>{allCars?.message}</p>
+          </div>
+        )}
       </div>
     </main>
   );
