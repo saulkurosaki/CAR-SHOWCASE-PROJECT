442ff1595548151196f6823c238655471c8f8f46
saulkurosaki <saulkurosaki13@gmail.com>
2024-05-19 10:18:04 -0600

    feat(CarDetails): implement the headlessui Dialog component along as its Transition component to create the modal content transition effects and the close modal button

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -2,7 +2,12 @@
 
 import { Fragment } from "react";
 import Image from "next/image";
-import { Dialog, Transition, TransitionChild } from "@headlessui/react";
+import {
+  Dialog,
+  DialogPanel,
+  Transition,
+  TransitionChild,
+} from "@headlessui/react";
 
 import { CarProps } from "@/types";
 interface CarDetailsProps {
@@ -27,6 +32,32 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
           >
             <div className="fixed inset-0 bg-black bg-opacity-25 " />
           </TransitionChild>
+
+          <div className="fixed inset-0 overflow-y-auto">
+            <div className="flex min-h-full items-center justify-center p-4 text-center">
+              <TransitionChild
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                <DialogPanel>
+                  <button type="button" onClick={closeModal}>
+                    <Image
+                      src="/close.svg"
+                      alt="close icon"
+                      width={20}
+                      height={20}
+                      className="object-contain"
+                    />
+                  </button>
+                </DialogPanel>
+              </TransitionChild>
+            </div>
+          </div>
         </Dialog>
       </Transition>
     </>
