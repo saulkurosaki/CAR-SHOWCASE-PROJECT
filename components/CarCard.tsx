00ce12fd53eb70b84636bdaf4a76a1dbae54d08c
saulkurosaki <saulkurosaki13@gmail.com>
2024-05-19 09:58:47 -0600

    feat(CarCard): implement the CarDetails component within the page

--- components/CarCard.tsx
+++ components/CarCard.tsx
@@ -6,6 +6,7 @@ import Image from "next/image";
 import { CarProps } from "@/types";
 import CustomButton from "./CustomButton";
 import { calculateCarRent } from "@/utils";
+import CarDetails from "./CarDetails";
 
 interface CarCardProps {
   car: CarProps;
@@ -77,6 +78,8 @@ const CarCard = ({ car }: CarCardProps) => {
           />
         </div>
       </div>
+
+      <CarDetails />
     </div>
   );
 };
