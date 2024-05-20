eefee452fb9256349ae1b077e4973d7082fbef86
saulkurosaki <saulkurosaki13@gmail.com>
2024-05-19 23:03:09 -0600

    feat(CarDetails): implement the complete CarDetails component

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -44,7 +44,7 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
                 leaveFrom="opacity-100 scale-100"
                 leaveTo="opacity-0 scale-95"
               >
-                <DialogPanel className="relative w-full max-w-lg max-h-[90vh] overflow-y-auto transform rounded-2xl bg-white text-left shadow-xl transition-all flex flex-col gap-5">
+                <DialogPanel className="relative w-full max-w-lg max-h-[90vh] overflow-y-auto transform rounded-2xl bg-white p-6 text-left shadow-xl transition-all flex flex-col gap-5">
                   <button
                     type="button"
                     onClick={closeModal}
