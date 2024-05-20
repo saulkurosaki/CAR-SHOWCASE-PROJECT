03bbb8f4b4e847b951f96d4f14ec3163fc7f79d6
saulkurosaki <saulkurosaki13@gmail.com>
2024-05-20 00:02:17 -0600

    feat(SearchBar): implement the SearchButton image

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -3,9 +3,18 @@
 import { useState } from "react";
 
 import SearchManufacturer from "./SearchManufacturer";
+import Image from "next/image";
 
 const SearchButton = ({ otherClasses }: { otherClasses: string }) => (
-  <button type="submit" className={`-ml-3 z-10 ${otherClasses}`}></button>
+  <button type="submit" className={`-ml-3 z-10 ${otherClasses}`}>
+    <Image
+      src="/magnifying-glass.svg"
+      alt="magnifying glass"
+      width={40}
+      height={40}
+      className="object-contain"
+    />
+  </button>
 );
 
 const SearchBar = () => {
