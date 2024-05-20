4a233cc987bc22047b6aef9d36fc540e96552a32
saulkurosaki <saulkurosaki13@gmail.com>
2024-05-20 15:06:36 -0600

    feat(ShowMore): implement the component container initial layout

--- components/ShowMore.tsx
+++ components/ShowMore.tsx
@@ -1,7 +1,15 @@
-import React from "react";
+"use client";
 
-const ShowMore = () => {
-  return <div>ShowMore</div>;
+import { useRouter } from "next/navigation";
+
+import { ShowMoreProps } from "@/types";
+
+const ShowMore = ({ pageNumber, isNext }: ShowMoreProps) => {
+  const router = useRouter();
+
+  const handleNavigation = () => {};
+
+  return <div className="w-full flex-center gap-5 mt-10"></div>;
 };
 
 export default ShowMore;
