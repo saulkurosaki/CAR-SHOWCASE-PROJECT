376633538d3d08edab5d565a13a4136b295e3ac0
saulkurosaki <saulkurosaki13@gmail.com>
2024-05-19 23:01:32 -0600

    feat(CarDetails): implement the car details modal info section info iteration

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -107,6 +107,22 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
                     <h2 className="font-semibold text-xl capitalize">
                       {car.make} {car.model}
                     </h2>
+
+                    <div className="mt-3 flex flex-wrap gap-4">
+                      {Object.entries(car).map(([key, value]) => (
+                        <div
+                          className="flex justify-between gap-5 w-full text-right"
+                          key={key}
+                        >
+                          <h4 className="text-grey capitalize">
+                            {key.split("_").join(" ")}
+                          </h4>
+                          <p className="text-black-100 font-semibold">
+                            {value}
+                          </p>
+                        </div>
+                      ))}
+                    </div>
                   </div>
                 </DialogPanel>
               </TransitionChild>
