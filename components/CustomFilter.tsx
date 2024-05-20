38e41a8ce4b17bf3889208d4ca49daea0cbddb4d
saulkurosaki <saulkurosaki13@gmail.com>
2024-05-20 14:55:59 -0600

    feat(CustomFilter): implement the completed filters functionality

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -12,26 +12,27 @@ import {
 } from "@headlessui/react";
 
 import { CustomFilterProps } from "@/types";
+import { updateSearchParams } from "@/utils";
 
 const CustomFilter = ({ title, options }: CustomFilterProps) => {
   const [selected, setSelected] = useState(options[0]);
   const router = useRouter();
 
-  const handleUpdateParams = (type: string, value: string) => {
-    const searchParams = new URLSearchParams(window.location.search);
-
-    searchParams.set(type, value);
-
-    const newPathname = `${
-      window.location.pathname
-    }?${searchParams.toString()}`;
+  const handleUpdateParams = (e: { title: string; value: string }) => {
+    const newPathname = updateSearchParams(title, e.value.toLowerCase());
 
     router.push(newPathname);
   };
 
   return (
     <div className="w-fit">
-      <Listbox value={selected} onChange={(e) => setSelected(e)}>
+      <Listbox
+        value={selected}
+        onChange={(e) => {
+          setSelected(e);
+          handleUpdateParams(e);
+        }}
+      >
         <div className="relative w-fit z-10">
           <ListboxButton className="custom-filter__btn">
             <span className="block truncate">{selected.title}</span>
