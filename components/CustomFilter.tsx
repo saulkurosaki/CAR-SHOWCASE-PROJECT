4ab75ade02b938053940a9fac0c2aeaf1e7e9b33
saulkurosaki <saulkurosaki13@gmail.com>
2024-05-20 13:58:39 -0600

    feat(CustomFilter): implement the list box layout, content and dynamic active properties

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -3,7 +3,13 @@
 import { Fragment, useState } from "react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
-import { Listbox, ListboxButton, Transition } from "@headlessui/react";
+import {
+  Listbox,
+  ListboxButton,
+  ListboxOption,
+  ListboxOptions,
+  Transition,
+} from "@headlessui/react";
 
 import { CustomFilterProps } from "@/types";
 
@@ -24,6 +30,28 @@ const CustomFilter = ({ title, options }: CustomFilterProps) => {
               className="ml-4 object-contain"
             />
           </ListboxButton>
+          <Transition
+            as={Fragment}
+            leave="transition ease-in duration-100"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <ListboxOptions className="custom-filter__options">
+              {options.map((option) => (
+                <ListboxOption
+                  key={option.title}
+                  value={option}
+                  className={({ active }) =>
+                    `relative cursor-default select-none py-2 px-4 ${
+                      active ? "bg-primary-blue text-white" : "text-gray-900"
+                    }`
+                  }
+                >
+                  {({ selected }) => <span>{option.title}</span>}
+                </ListboxOption>
+              ))}
+            </ListboxOptions>
+          </Transition>
         </div>
       </Listbox>
     </div>
