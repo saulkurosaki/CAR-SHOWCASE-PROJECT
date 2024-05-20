416dd28dcaf4ed355b14a5c7e3f7ea71074f2728
saulkurosaki <saulkurosaki13@gmail.com>
2024-05-20 13:46:32 -0600

    feat(CustomFilter): implement the headlessui Listbox component selected state value along as the Listbox title

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -3,15 +3,21 @@
 import { Fragment, useState } from "react";
 import Image from "next/image";
 import { useRouter } from "next/navigation";
-import { Listbox, Transition } from "@headlessui/react";
+import { Listbox, ListboxButton, Transition } from "@headlessui/react";
 
 import { CustomFilterProps } from "@/types";
 
 const CustomFilter = ({ title, options }: CustomFilterProps) => {
+  const [selected, setSelected] = useState(options[0]);
+
   return (
     <div className="w-fit">
-      <Listbox>
-        <div className="relative w-fit z-10"></div>
+      <Listbox value={selected} onChange={(e) => setSelected(e)}>
+        <div className="relative w-fit z-10">
+          <ListboxButton className="custom-filter__btn">
+            <span className="block truncate">{selected.title}</span>
+          </ListboxButton>
+        </div>
       </Listbox>
     </div>
   );
