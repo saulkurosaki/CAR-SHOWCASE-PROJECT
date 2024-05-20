59711483b4257a0dd3ddf7782ee6412620367292
saulkurosaki <saulkurosaki13@gmail.com>
2024-05-20 10:59:45 -0600

    feat(CustomFilter): implement the headlessui Listbox component as well as its container initial layout

--- components/CustomFilter.tsx
+++ components/CustomFilter.tsx
@@ -1,7 +1,20 @@
-import React from "react";
+"use client";
 
-const CustomFilter = () => {
-  return <div>CustomFilter</div>;
+import { Fragment, useState } from "react";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { Listbox, Transition } from "@headlessui/react";
+
+import { CustomFilterProps } from "@/types";
+
+const CustomFilter = ({ title, options }: CustomFilterProps) => {
+  return (
+    <div className="w-fit">
+      <Listbox>
+        <div className="relative w-fit z-10"></div>
+      </Listbox>
+    </div>
+  );
 };
 
 export default CustomFilter;
