1569b5a38331e66458829c4a0abd61d1007e2f36
saulkurosaki <saulkurosaki13@gmail.com>
2024-05-20 15:15:05 -0600

    feat(ShowMore): implement the handleNavigation function

--- components/ShowMore.tsx
+++ components/ShowMore.tsx
@@ -4,11 +4,17 @@ import { useRouter } from "next/navigation";
 
 import { ShowMoreProps } from "@/types";
 import CustomButton from "./CustomButton";
+import { updateSearchParams } from "@/utils";
 
 const ShowMore = ({ pageNumber, isNext }: ShowMoreProps) => {
   const router = useRouter();
 
-  const handleNavigation = () => {};
+  const handleNavigation = () => {
+    const newLimit = (pageNumber + 1) * 10;
+    const newPathname = updateSearchParams("limit", `${newLimit}`);
+
+    router.push(newPathname);
+  };
 
   return (
     <div className="w-full flex-center gap-5 mt-10">
