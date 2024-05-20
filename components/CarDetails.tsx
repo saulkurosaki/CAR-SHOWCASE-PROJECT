c6cdb9afa563b95a5582a0b1860b569685ee67f6
saulkurosaki <saulkurosaki13@gmail.com>
2024-05-19 22:54:41 -0600

    feat(CarDetails): implement the car details modal info section car title

--- components/CarDetails.tsx
+++ components/CarDetails.tsx
@@ -102,6 +102,12 @@ const CarDetails = ({ isOpen, closeModal, car }: CarDetailsProps) => {
                       </div>
                     </div>
                   </div>
+
+                  <div className="flex-1 flex flex-col gap-2">
+                    <h2 className="font-semibold text-xl capitalize">
+                      {car.make} {car.model}
+                    </h2>
+                  </div>
                 </DialogPanel>
               </TransitionChild>
             </div>
